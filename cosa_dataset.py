d9caf94fe20dc1f99d69350316b919c05174c3a5
Jenny Huang <hqjenny@hotmail.com>
2021-07-19 19:57:55 -0700

    Add training and test split to dataset

--- cosa_dataset.py
+++ cosa_dataset.py
@@ -4,6 +4,7 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
@@ -20,11 +21,25 @@ class CoSADataset(Dataset):
             csv_file (string): Path to the csv file with annotations.
         """
         csv_file = f'{root}/arch_dataset_12.csv'
-        print(csv_file)
-        self.arch_feats_frame = pd.read_csv(csv_file)
+        print(f"Path to CSV file: {csv_file}")
+        full_dataset = shuffle(pd.read_csv(csv_file), random_state=0)
         self.transform = transform
         self.target_transform = target_transform
 
+        num_rows = len(full_dataset)
+        train_part = int (0.75 * num_rows)
+        test_part = int (0.15 * num_rows)
+        if split == "train":
+            self.arch_feats_frame = full_dataset[0: train_part]
+        elif split == "valid":
+            self.arch_feats_frame = full_dataset[train_part:-test_part]
+        elif split == "test":
+            self.arch_feats_frame = full_dataset[-test_part:]
+        else:
+            self.arch_feats_frame = full_dataset
+        dataset_size = len(self.arch_feats_frame)
+        print(f"Load dataset with {dataset_size} entries.")
+        
     def __len__(self):
         return len(self.arch_feats_frame)
 
@@ -34,6 +49,7 @@ class CoSADataset(Dataset):
 
         arch_feats = self.arch_feats_frame.iloc[idx, 3:]
         label = self.arch_feats_frame.iloc[idx, 1]
+
         print(f'cycle: {label}')
         arch_feats = np.array([arch_feats])
         arch_feats = arch_feats.astype('float').reshape(-1, ).astype(np.float64)
@@ -44,3 +60,7 @@ class CoSADataset(Dataset):
             label = self.target_transform(label)
 
         return arch_feats, label
+
+if __name__ == "__main__":
+    train_dataset = CoSADataset(root = './', split= "train", )
+    test_dataset = CoSADataset(root = './', split= "test",)
